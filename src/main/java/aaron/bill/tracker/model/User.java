468fa8378db07b66587ddf5c22de5aa58d385bb8
Aaron Ellis <aaronellis1@hotmail.co.uk>
2021-06-10 08:07:13 +0100

    [2] Created user model

---
+++ src/main/java/aaron/bill/tracker/model/User.java
@@ -0,0 +1,42 @@
+package aaron.bill.tracker.model;
+
+public class User {
+
+	
+	
+	private String id;
+	private String username;
+	private String email;
+	private String password;
+	
+	public String getId()
+	{
+		return this.id;
+	}
+	
+	public String getUsername()
+	{
+		return this.username;
+	}
+	
+	public void setUsername(String newUsername)
+	{
+		this.username = newUsername;
+	}
+	
+	public String getEmail() 
+	{
+		return this.email;
+	}
+	
+	public void setEmail(String newEmail)
+	{
+		this.email = newEmail;
+	}
+	
+	public String getPassword()
+	{
+		return this.password;
+	}
+	
+}
